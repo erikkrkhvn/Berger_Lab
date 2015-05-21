3c65f25cab2c81bc77df8907d7ec62c1907b23fd
erikkrkhvn <erik.krkhvn@gmail.com>
2015-05-20 23:27:19 -0400

    Final test passed

--- difference.py
+++ difference.py
@@ -18,18 +18,25 @@ def fileinput(filename, col1, col2):
 		num2 = int(col2)
 		name_col1 = parsed[(3+num1)]
 		name_col2 = parsed[(3+num2)]
+		new_first = list()
+		new_first.append("")
+		for b in range(0,3):
+			new_first.append(parsed[b])
+		new_first.append(("%s-%s" % (name_col1,name_col2)))
 		with open(("Difference_%s_%s.txt" % (name_col1,name_col2)), "w") as write_file:
-			write_file.write(("		Chr	Start	End	%s" % ("%s-%s" % (name_col1,name_col2)))+"\n") 
-			while True:
+			write_file.write(("\t".join(new_first)) +"\n") 
+			a = 0
+			while a<100:
 				next_line = read_file.readline()
-				if next_line = "":
+				if next_line == "":
 					break
 				pieces = next_line.split()
 				new_value = int(pieces[(4+num1)]) - int(pieces[(4+num2)])
-				new_line = list(5)
-				for i in range(0,3):
-					new_line[i] = pieces[i]
-				new_line[4] = int(new_value)
-				write_file.write("\t".join(new_line))
+				new_line = list()
+				for i in range(0,4):
+					new_line.append(pieces[i])
+				new_line.append(str(new_value))
+				write_file.write(("\t".join(new_line)) + "\n")
+				a = a+1
 
 fileinput(filename, col1, col2)
