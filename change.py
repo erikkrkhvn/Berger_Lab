8a6247db456a4706e91d630aa211b7158b03d85d
erikkrkhvn <erik.krkhvn@gmail.com>
2015-05-21 14:40:15 -0400

    double class implemented

--- change.py
+++ change.py
@@ -20,56 +20,60 @@ class file:
 
 	def __init__ (self, filename):
 		self.filename = filename
+		with open(self.filename, "r") as read_file:
+			self.first_line = read_file.readline()
 
-	def difference(self, col1, col2):
+	def normalize(self, norm_const):
+		norm = 1
+
+class difference(file):
+
+	def __init__ (self, file):
+		self.filename = file.filename
+		self.first_line = file.first_line
+		parsed = self.first_line.split()
+		col1 = input1
+		col2 = sys.argv[4]
+		self.num1 = int(col1)
+		self.num2 = int(col2)
+		self.name_col1 = parsed[(2+self.num1)]
+		self.name_col2 = parsed[(2+self.num2)]
+		self.new_first = list()
+		self.new_first.append("")
+		for b in range(0,3):
+			self.new_first.append(parsed[b])
+		
+	def subtraction(self):
 		with open(self.filename, "r") as read_file:
 			first_line = read_file.readline()
-			parsed = first_line.split()
-			num1 = int(col1)
-			num2 = int(col2)
-			name_col1 = parsed[(2+num1)]
-			name_col2 = parsed[(2+num2)]
-			self.new_first = list()
-			self.new_first.append("")
-			for b in range(0,3):
-				self.new_first.append(parsed[b])
-			self.new_first.append(("%s-%s" % (name_col1,name_col2)))
-			with open(("Difference_%s_%s.txt" % (name_col1,name_col2)), "w") as write_file:
+			self.new_first.append(("%s-%s" % (self.name_col1, self.name_col2)))
+			with open(("Difference_%s_%s.txt" % (self.name_col1, self.name_col2)), "w") as write_file:
 				write_file.write(("\t".join(self.new_first)) +"\n") 
 				while True:
 					next_line = read_file.readline()
 					if next_line == "":
 						break
 					pieces = next_line.split()
-					new_value = int(pieces[(3+num1)]) - int(pieces[(3+num2)])
+					new_value = int(pieces[(3+self.num1)]) - int(pieces[(3+self.num2)])
 					new_line = list()
 					for i in range(0,4):
 						new_line.append(pieces[i])
 					new_line.append(str(new_value))
 					write_file.write(("\t".join(new_line)) + "\n")
 
-	def division(self, col1, col2):
+	def division(self):
 		with open(self.filename, "r") as read_file:
 			first_line = read_file.readline()
-			parsed = first_line.split()
-			num1 = int(col1)
-			num2 = int(col2)
-			name_col1 = parsed[(2+num1)]
-			name_col2 = parsed[(2+num2)]
-			self.new_first = list()
-			self.new_first.append("")
-			for b in range(0,3):
-				self.new_first.append(parsed[b])
-			self.new_first.append(("%s/%s" % (name_col1,name_col2)))
-			with open(("Division_%s_%s.txt" % (name_col1,name_col2)), "w") as write_file:
+			self.new_first.append(("%s/%s" % (self.name_col1, self.name_col2)))
+			with open(("Division_%s_%s.txt" % (self.name_col1, self.name_col2)), "w") as write_file:
 				write_file.write(("\t".join(self.new_first)) +"\n") 
 				while True:
 					next_line = read_file.readline()
 					if next_line == "":
 						break
 					pieces = next_line.split()
-					first_num = int(pieces[(3+num1)])
-					second_num = int(pieces[(3+num2)])
+					first_num = int(pieces[(3+self.num1)])
+					second_num = int(pieces[(3+self.num2)])
 					if second_num == 0:
 						new_value = first_num
 					else:
@@ -79,15 +83,17 @@ class file:
 						new_line.append(pieces[i])
 					new_line.append(str(new_value))
 					write_file.write(("\t".join(new_line)) + "\n")
-	def normalize(self, norm_const):
-		norm = 1
+
+	def while_loop():
+		
 
 
-
-new_file = file(filename)			
+first = file(filename)			
 if action == "s":
-	new_file.difference(input1, sys.argv[4])
+	second = difference(first)
+	second.subtraction()
 elif action == "q":
-	new_file.division(input1, sys.argv[4])
+	second = difference(first)
+	second.division()
 elif action == "n":
 	new_file.normalize(input1)
