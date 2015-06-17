8b89cbf668864daf01b243f59203a78cbcf18c17
erikkrkhvn <erik.krkhvn@gmail.com>
2015-06-17 12:53:18 -0400

    plotting added

--- example.py
+++ example.py
@@ -1,9 +1,9 @@
 import pandas as pd 
 import sys
+import matplotlib.pyplot as plt
 
 filename = sys.argv[1]
 action = sys.argv[2]
-input1 = sys.argv[3]
 
 class file:
 
@@ -11,7 +11,7 @@ class file:
         self.data = pd.read_csv(filename, delim_whitespace = True)
 
     def sub(self):
-        self.num_of_col = int(input1)
+        self.num_of_col = int(sys.argv[3])
         self.col_list = list()
         self.name_list = list()
         for i in range(0,self.num_of_col):
@@ -25,11 +25,22 @@ class file:
             new_name = a+"-"+b
             self.data[new_name] = self.data.apply(lambda x: x[a] - x[b], axis = 1)
             self.data = self.data.drop([a,b], axis = 1)
-        self.data.to_csv("sub_newfile.txt", sep = '\t', index = False)
+        self.data.to_csv("sub_newfile.txt", sep = '\t')
 
     def norm(self):
-        
+        self.name_list = list(self.data.columns.values)
+        for i in range(3, len(self.data.columns)):
+            self.data[self.name_list[i]] = self.data[self.name_list[i]]*float(int(sys.argv[3])/self.data[self.name_list[i]].sum())
+        self.data.to_csv("norm_newfile.txt", sep = '\t')
+
+    def plot(self):
+        self.data.plot(x = 'Start', kind = 'bar', subplots = True, by = ['Chr'])
+        plt.show()
 
 first = file()
 if action == "s":
     first.sub()
+if action == "n":
+    first.norm()
+if action == "p":
+    first.plot()
